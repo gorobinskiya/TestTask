b87ad2addbd5a1e0b9cf5ca7c95dcb34fcef39e2
gorobinskiya <gorobinskiya5@gmail.com>
2023-04-25 12:51:21 +0300

    Implemented fragments

--- app/src/main/java/com/example/testtask/presentation/MainActivity.kt
+++ app/src/main/java/com/example/testtask/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.testtask
+package com.example.testtask.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testtask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/testtask/presentation/main/MainFragment.kt
@@ -0,0 +1,18 @@
+package com.example.testtask.presentation.mainpackage
+
+import com.example.testtask.R
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
